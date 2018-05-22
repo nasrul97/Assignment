fc150ff1c0b5727164059e2194a0def05f85e208
nasrul97 <m.nasrulazwan97@gmail.com>
2018-05-22 16:35:52 +0800

    Java Time Client

---
+++ timeC.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+
+public class timeC{public static void main(String[] args) throws 
+Exception{ 
+
+System.out.println("Server Time >>>>");
+
+DatagramSocket cs=new DatagramSocket();
+
+InetAddress ip=InetAddress.getByName("192.168.75.130");
+
+byte[] rd=new byte[100];
+byte[] sd=new byte[100];
+
+DatagramPacket sp=new DatagramPacket(sd,sd.length,ip,7777);
+
+DatagramPacket rp=new DatagramPacket(rd,rd.length);
+
+cs.send(sp);
+
+cs.receive(rp);
+
+String time=new String(rp.getData());
+
+System.out.println(time);
+
+cs.close();
+
+}
+
+}
